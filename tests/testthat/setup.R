c3c1894e536e5903f4fc7fef3385ecaf8954b207
john <bblodfon@gmail.com>
2025-09-04 15:00:22 +0200

    add mirai to tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,13 +1,6 @@
-old_opts = options(
-  warnPartialMatchArgs = TRUE,
-  warnPartialMatchAttr = TRUE,
-  warnPartialMatchDollar = TRUE
-)
-
-# https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
-old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
-
+lg = lgr::get_logger("mlr3")
 old_threshold = lg$threshold
-old_plan = future::plan()
-lg$set_threshold(0)
-future::plan("sequential")
+lg$set_threshold("warn")
+
+mirai::daemons(0, .compute = "mlr3_encapsulation")
+mirai::daemons(1, .compute = "mlr3_encapsulation")
--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,3 +1,3 @@
-options(old_opts)
 lg$set_threshold(old_threshold)
-future::plan(old_plan)
+
+mirai::daemons(0, .compute = "mlr3_encapsulation")
