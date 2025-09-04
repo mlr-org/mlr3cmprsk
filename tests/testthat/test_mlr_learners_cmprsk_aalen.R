0f6bcbcd493ec2f02e4d26e1bb26b581cf79220a
john <bblodfon@gmail.com>
2025-09-04 21:19:51 +0200

    update Rbuildignore file

--- tests/testthat/test_mlr_learners_cmprsk_aalen.R
+++ tests/testthat/test_mlr_learners_cmprsk_aalen.R
@@ -3,6 +3,6 @@ test_that("autotest", {
     learner = lrn("cmprsk.aalen")
     expect_learner(learner)
     result = run_autotest(learner, N = 42, check_replicable = FALSE)
-    expect_true(result, info = result$error)
+    expect_true(result)
   })
 })
