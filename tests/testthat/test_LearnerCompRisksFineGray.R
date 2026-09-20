a56c4cb2ff958344f3f75f66fa5740ea26031e7e
john <bblodfon@gmail.com>
2026-09-20 22:02:57 +0200

    fix cencode param test

--- tests/testthat/test_LearnerCompRisksFineGray.R
+++ tests/testthat/test_LearnerCompRisksFineGray.R
@@ -48,6 +48,7 @@ test_that("train params of cmprsk.fg", {
     "tf", # not supported
     "failcode", # handled by mlr3
     "cencode", # handled by mlr3
+    "cengroup", # not supported
     "subset", # handled by mlr3
     "na.action" # not supported
   )
