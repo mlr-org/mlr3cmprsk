fbd3a66d2df67a6799a029fd8dd79016800d0249
john <bblodfon@gmail.com>
2026-09-20 23:40:14 +0200

    simplify and improve the helper expectation functions

--- inst/testthat/helper_expectations.R
+++ inst/testthat/helper_expectations.R
@@ -3,38 +3,32 @@ expect_task_cmprsk = function(task) {
   expect_task(task)
   expect_task_supervised(task)
   expect_class(task$truth(), "Surv")
+  expect_equal(task$task_type, "cmprsk")
 
   f = task$formula()
   expect_formula(f)
   expect_setequal(mlr3misc::extract_vars(f)$lhs, task$target_names)
   expect_class(task$aalen_johansen(), "survfit")
+  expect_numeric(task$event(), lower = 0, finite = TRUE, any.missing = FALSE)
+  expect_numeric(task$times(), lower = 0, finite = TRUE, any.missing = FALSE)
+  expect_numeric(task$unique_times(), unique = TRUE, any.missing = FALSE)
+  expect_numeric(task$unique_event_times(), unique = TRUE, any.missing = FALSE)
+  expect_number(task$cens_prop(), lower = 0, upper = 1, finite = TRUE, any.missing = FALSE)
+  expect_in(task$cens_type, c("right"))
+  expect_character(task$cmp_events, any.missing = FALSE)
 }
 
 expect_prediction_cmprsk = function(p) {
+  testthat::expect_output(print(p), "Prediction")
+  expect_data_table(data.table::as.data.table(p), nrows = length(p$row_ids))
+  expect_integerish(p$missing)
   expect_r6(p, "Prediction", public = c("row_ids", "truth", "predict_types", "cif"))
-  n_obs = length(p$row_ids)
 
+  n_obs = length(assert_row_ids(p$row_ids))
   if (n_obs > 0) {
-    # convert to data.table
-    tab = as.data.table(p)
-
-    expect_data_table(tab, nrows = n_obs, ncols = 4, col.names = "named")
-    expect_set_equal(names(tab), c("row_ids", "time", "event", "CIF"))
-    expect_list(tab$CIF, len = n_obs, any.missing = FALSE, null.ok = FALSE)
-    # Assert that every element of tab$CIF (i.e. corresponding to each observation)
-    # is a list containing numeric CIF vectors, one per competing risk
-    n_cmp_events = length(attr(p$truth, "states"))
-    expect_true(all(sapply(tab$CIF, function(x) {
-      check_list(x, len = n_cmp_events) && all(sapply(x, is.numeric))
-    })))
-
-    expect_atomic_vector(p$missing)
-
-    # check that CIF data list makes sense
-    if ("cif" %in% p$predict_types && !is.null(p$data$cif)) {
-      check_prediction_data(p$data, n_rows = n_obs, n_cmp_events = n_cmp_events)
-      # TODO: check for `survdistr` class when `p$cif` returns that
-    }
+    assert_surv(p$truth, len = n_obs)
+    causes = attr(p$truth, "states")
+    assert_cif_list(p$cif, n_rows = n_obs, n_causes = length(causes))
   }
 
   expect_class(p, "PredictionCompRisks")
