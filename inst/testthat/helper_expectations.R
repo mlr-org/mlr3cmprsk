15e09c473fb1ddf5ba973b69a5d76d6b649068c3
john <bblodfon@gmail.com>
2026-09-24 12:45:14 +0200

    better helper prediction expectation function

--- inst/testthat/helper_expectations.R
+++ inst/testthat/helper_expectations.R
@@ -13,23 +13,25 @@ expect_task_cmprsk = function(task) {
   expect_numeric(task$times(), lower = 0, finite = TRUE, any.missing = FALSE)
   expect_numeric(task$unique_times(), unique = TRUE, any.missing = FALSE)
   expect_numeric(task$unique_event_times(), unique = TRUE, any.missing = FALSE)
-  expect_number(task$cens_prop(), lower = 0, upper = 1, finite = TRUE, any.missing = FALSE)
+  expect_number(task$cens_prop(), lower = 0, upper = 1, finite = TRUE)
   expect_in(task$cens_type, c("right"))
   expect_character(task$cmp_events, any.missing = FALSE)
 }
 
 expect_prediction_cmprsk = function(p) {
+  expect_r6(
+    p,
+    classes = c("Prediction", "PredictionCompRisks"),
+    public = c("row_ids", "truth", "predict_types", "cif")
+  )
+  expect_r6(
+    p$data,
+    classes = c("PredictionData", "PredictionDataCompRisks"),
+    public = c("row_ids", "truth", "cif")
+  )
   testthat::expect_output(print(p), "Prediction")
   expect_data_table(data.table::as.data.table(p), nrows = length(p$row_ids))
-  expect_integerish(p$missing)
-  expect_r6(p, "Prediction", public = c("row_ids", "truth", "predict_types", "cif"))
-
-  n_obs = length(assert_row_ids(p$row_ids))
-  if (n_obs > 0) {
-    assert_surv(p$truth, len = n_obs)
-    causes = attr(p$truth, "states")
-    assert_cif_list(p$cif, n_rows = n_obs, n_causes = length(causes))
-  }
-
-  expect_class(p, "PredictionCompRisks")
+  assert_surv(p$truth, len = length(p$row_ids))
+  assert_list(p$cif, types = "matrix", len = length(attr(p$truth, "states")))
+  expect_setequal(names(p$cif), attr(p$truth, "states"))
 }
